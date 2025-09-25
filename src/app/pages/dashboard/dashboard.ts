36f8ae3b9dcb08ca2dd42acb4ae578762635b75b
Oscar Cordoba <ocordoba3@hotmail.com>
2025-09-25 11:52:40 -0500

    color palette improved

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { paths } from '@app/utils/paths';
 
 @Component({
   selector: 'app-dashboard',
   imports: [MatIconModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss'
+  styleUrl: './dashboard.scss',
 })
 export default class Dashboard {
+  PATHS = paths;
+  router = inject(Router);
 
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
 }
--- src/app/shared/gif-item/gif-item.ts
+++ src/app/shared/gif-item/gif-item.ts
@@ -1,12 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { LocalGifInterface } from '@app/interfaces/local-gif.interface';
 
 @Component({
   selector: 'app-gif-item',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './gif-item.html',
   styleUrl: './gif-item.scss',
 })
 export class GifItem {
   gif = input<LocalGifInterface>();
+  isCopied = signal(false);
+  copyText = computed(() => (this.isCopied() ? '¡Copiado!' : 'Copiar link'));
+
+  copyToClipboard(url: string) {
+    navigator.clipboard.writeText(url);
+    this.isCopied.set(true);
+    setTimeout(() => this.isCopied.set(false), 2000);
+  }
 }
